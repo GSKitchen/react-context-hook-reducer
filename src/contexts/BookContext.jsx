8556a9bdaeded4b9afca5b1f5843ab7de3e59e3c
Sabbir Ahmad <sabbir.sio@gmail.com>
2019-11-29 19:02:33 +0530

    Implemented Local Storage

--- src/contexts/BookContext.jsx
+++ src/contexts/BookContext.jsx
@@ -1,14 +1,25 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useReducer, useEffect } from "react";
 import { bookReducer } from "../reducers/BookReducer";
 
 export const BookContext = createContext();
 
 const BookContextProvider = props => {
-  const [books, dispatch] = useReducer(bookReducer, [
-    { title: "Kabababu", author: "Sunil Ganguly", id: 1 },
-    { title: "Sonar Kella", author: "Satyajit Roy", id: 2 },
-    { title: "Kalbela", author: "Samaresh Majumdar", id: 3 }
-  ]);
+  const [books, dispatch] = useReducer(
+    bookReducer,
+    [
+      { title: "Kabababu", author: "Sunil Ganguly", id: 1 },
+      { title: "Sonar Kella", author: "Satyajit Roy", id: 2 },
+      { title: "Kalbela", author: "Samaresh Majumdar", id: 3 }
+    ],
+    () => {
+      const localData = localStorage.getItem("books");
+      return localData ? JSON.parse(localData) : [];
+    }
+  );
+
+  useEffect(() => {
+    localStorage.setItem("books", JSON.stringify(books));
+  }, [books]);
 
   return (
     <BookContext.Provider value={{ books, dispatch }}>
